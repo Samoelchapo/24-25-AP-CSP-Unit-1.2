c380cda4504fb30ab90255912284e1bad88d7307
20556 <samuel.meneses@vtsd.com>
2024-10-24 12:25:04 -0400

    Unit 1-2-1 day 1

---
+++ 1-2-1/step1.py
@@ -0,0 +1,22 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+
+
+#-----game configuration----
+spot_color= "pink"
+spot_size = 2
+spot_shape= "circle"
+spot_movement="left"
+#-----initialize turtle-----0
+spot= trtl.Turtle()
+spot.shape(spot_shape)
+spot.shapesize(spot_size)
+spot.color(spot_color)
+
+#-----game functions--------
+
+
+#-----events----------------
+wn= trtl.Screen()
+wn.mainloop()
