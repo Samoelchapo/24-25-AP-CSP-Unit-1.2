962ab8554b54f8ba6250db6830cc41a7e41a30c0
20556 <samuel.meneses@vtsd.com>
2024-10-31 11:46:15 -0400

    clicker game

--- 1-2-1/step1.py
+++ 1-2-1/step1.py
@@ -4,13 +4,16 @@ import turtle as trtl
 import random as rand
 
 from random import randint
-font_setup= ("Arial")
+font_setup= ("Arial", 20, "normal")
 clicks = 1
 score=0
+timer=30
+counter_interval=10000
+timer_up= False
 
 #-----game configuration----
-spot_color= "pink"
-spot_size = 2
+spot_color= "gold"
+spot_size = 5
 spot_shape= "circle"
 spot_movement="left"
 #-----initialize turtle-----0
@@ -21,6 +24,13 @@ spot.color(spot_color)
 spot.speed("fastest")
 
 score_writer= trtl.Turtle()
+score_writer.hideturtle()
+score_writer.speed("fastest")
+
+score_box=trtl.Turtle()
+score_box.hideturtle()
+
+counter=trtl.Turtle()
 
 #-----game functions--------
 spot.penup()
@@ -31,15 +41,38 @@ def change_position():
 def spot_clicked(x,y):
        change_position()
        update_score()
-
 def update_score():
     global score
+    score_writer.clear()
     score += 1
-    print(score)
+    score_writer.write(score,font=font_setup)
+    score_writer.penup()
+    score_writer.goto(-430,330)
+    score_writer.pendown()
 
-score_writer.penup()
-score_writer.goto(-400,350)
-score_writer.pendown()
+def countdown():
+        global timer, timer_up
+        counter.clear()
+        if timer <= 0:
+            counter.write("Time's Up", font=font_setup)
+            timer_up = True
+        else:
+            counter.write("Timer: " + str(timer), font=font_setup)
+            timer -= 1
+            counter.getscreen().ontimer(countdown, counter_interval)
+counter.penup()
+counter.goto(250,300)
+counter.pendown()
+
+score_box.speed("fastest")
+score_box.penup()
+score_box.goto(-400,330)
+score_box.pendown()
+score_box.goto(-370,330)
+score_box.goto(-370,370)
+score_box.goto(-440,370)
+score_box.goto(-440,330)
+score_box.goto(-400,330)
 
 
 
@@ -47,4 +80,5 @@ score_writer.pendown()
 #-----events----------------
 spot.onclick(spot_clicked)
 wn= trtl.Screen()
+wn.ontimer(countdown,counter_interval)
 wn.mainloop()
