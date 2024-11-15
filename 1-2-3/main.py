79fece7fde1ff518939709eacbd95b89ea1bb098
20556 <samuel.meneses@vtsd.com>
2024-11-15 13:10:36 -0500

    1-2-3

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -1,4 +1,4 @@
-#   a123_apple_1.py
+
 import turtle as trtl
 
 #-----setup-----
@@ -9,22 +9,27 @@ wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 wn.bgpic("background.gif")
 apple = trtl.Turtle()
-
+drawer = trtl.Turtle()
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
-def drop_apple(active_apple):
+def drop_apple():
   apple.penup()
-  x_coor = active_apple.xcor()
-  y_coor = active_apple.ycor()
-  active_apple.goto(x_coor, y_coor - 300)
+  x_coor = apple.xcor()
+  y_coor = apple.ycor()
+  apple.goto(x_coor, y_coor - 300)
   apple.pendown()
+
 
 #-----function calls-----
 draw_apple(apple)
-drop_apple(apple)
 
+
+
+wn.onkeypress(drop_apple, "a")
+
+wn.listen()
 wn.mainloop()
