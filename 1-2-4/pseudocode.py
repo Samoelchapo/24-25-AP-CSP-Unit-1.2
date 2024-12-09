73b1a4800653a398663b3a83f7f03869f18985b4
20556 <samuel.meneses@vtsd.com>
2024-12-09 12:06:40 -0500

    Github Fix

---
+++ 1-2-4/pseudocode.py
@@ -0,0 +1,69 @@
+
+import turtle as trtl
+import random as rand
+
+#variables
+
+
+wn = trtl.Screen()
+maze_painter = trtl.Turtle()
+wall_len=35
+path_width=30
+
+
+
+#setup turtle
+maze_painter.left(90)
+maze_painter.pensize(5)
+maze_painter.speed(0)
+
+#draw maze
+
+#process:
+#draw a line
+
+#Turn left
+#increment length
+#repeat
+def draw_barrier():
+    maze_painter.right(90)
+    maze_painter.forward(path_width)
+    maze_painter.backward(path_width)
+    maze_painter.left(90)
+
+
+
+for wall in range(21):
+    first_length = wall_len/rand.randint(1,5)
+    maze_painter.forward(first_length)
+    maze_painter.penup()
+    maze_painter.forward(path_width)
+    maze_painter.pendown()
+    if wall>5 :
+        draw_barrier()
+    maze_painter.forward(wall_len - path_width - first_length)
+    maze_painter.left(90)
+    wall_len += 15
+
+
+
+
+# randomize location of doors and barriers in wall
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wn.listen()
+wn.mainloop()
---
+++ 1-2-5/game_project.py
@@ -0,0 +1,9 @@
+import turtle as trtl
+
+
+
+#player list
+Player1_score=0
+Player2_score=0
+
+
