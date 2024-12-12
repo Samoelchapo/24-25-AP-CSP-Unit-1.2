d1bf640e420b4fec1159a90bd777acaef7cce1d0
20556 <samuel.meneses@vtsd.com>
2024-12-12 13:12:16 -0500

    Github Fix

--- 1-2-5/game_project.py
+++ 1-2-5/game_project.py
@@ -1,49 +1,93 @@
 import turtle
 import turtle as trtl
-pong_ball=turtle.Turtle()
+
+
 wn = trtl.Screen()
 wn.bgcolor('lightblue')
-
+ball = turtle.Turtle()
 #player list
 Player1_score=0
 Player2_score=0
-
-#game configuration
-
-
-
-
-
+score = turtle.Turtle()
+score.hideturtle()
+score.speed(0)
+game_over = False
+winner = None
 
 
-#left paddle
-leftpaddle=trtl.Turtle()
-leftpaddle.speed(0)
-leftpaddle.penup()
-leftpaddle.goto(-400,0)
-#left paddle shape/size
-leftpaddle.shape("square")
-leftpaddle.color("white")
-leftpaddle.shapesize(stretch_wid=5,stretch_len=1)
+score.color("black")
+score.penup()
+score.goto(-250, 300)
+score.write("Player 1 score : 0   Player 2 score: 0",
+            font=("arial", 24, "normal"))
 
 
-#right paddle
-rightpaddle = trtl.Turtle()
-rightpaddle.speed(0)
-rightpaddle.penup()
-rightpaddle.goto(400,0)
-
-#right paddle shape/size
-rightpaddle.shape("square")
-rightpaddle.color("white")
-rightpaddle.shapesize(stretch_wid=5,stretch_len=1)
 
 
-#pong
-pong_ball.goto(0,0)
-pong_ball.shape("circle")
-pong_ball.color("white")
-pong_ball.shapesize(stretch_wid=1,stretch_len=1)
+
+
+def leftstick():
+    leftpaddle = trtl.Turtle()
+    leftpaddle.speed(0)
+    leftpaddle.penup()
+    leftpaddle.goto(-400,0)
+    leftpaddle.shape("square")
+    leftpaddle.color("white")
+    leftpaddle.shapesize(stretch_wid=5,stretch_len=1)
+
+
+leftstick()
+
+def rightstick():
+    rightpaddle = trtl.Turtle()
+    rightpaddle.speed(0)
+    rightpaddle.penup()
+    rightpaddle.goto(400,0)
+    rightpaddle.shape("square")
+    rightpaddle.color("white")
+    rightpaddle.shapesize(stretch_wid=5,stretch_len=1)
+
+rightstick()
+
+
+
+# Update score display
+
+
+def leftstick_up():
+    leftstick.
+def leftstick_down():
+    if wn.onkeypress(leftstick, "s"):
+        leftstick.down = -10
+def rightstick_up():
+    if wn.onkeypress(rightstick, "Up"):
+        rightstick.up = 10
+
+def rightstick_down():
+    if wn.onkeypress(rightstick, "Down"):
+        rightstick.down = -10
+
+# Set up keyboard bindings
+wn.listen()
+wn.onkeypress(leftstick_up, "w")
+wn.onkeypress(leftstick_down, "s")
+wn.onkeypress(rightstick_up, "Up")
+wn.onkeypress(rightstick_down, "Down")
+
+
+
+
+
+
+
+
+
+ball.shape("circle")
+ball.color("white")
+ball.goto(0,0)
+ball.shape("circle")
+ball.color("white")
+ball.shapesize(stretch_wid=1,stretch_len=1)
 
 
 
